bcbf9c9bf0c8a3c86a96fff1908ab2168d81f398
J.Kholikov <jabbeerka@gmail.com>
2020-08-17 20:15:47 +0300

    Updates

---
+++ precache-manifest.5e1b9cc2a7906d0d1323a59e6c6a8116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ebbce69259873efce8",
+    "url": "/twitter/static/css/main.d040a755.chunk.css"
+  },
+  {
+    "revision": "31ebbce69259873efce8",
+    "url": "/twitter/static/js/main.31ebbce6.chunk.js"
+  },
+  {
+    "revision": "25e971f46237d339c6da",
+    "url": "/twitter/static/css/1.56bb7743.chunk.css"
+  },
+  {
+    "revision": "25e971f46237d339c6da",
+    "url": "/twitter/static/js/1.25e971f4.chunk.js"
+  },
+  {
+    "revision": "3f89907d52536c66f970",
+    "url": "/twitter/static/js/runtime~main.3f89907d.js"
+  },
+  {
+    "revision": "70a3d2c7211ee221cd3c58aaffe71e5c",
+    "url": "/twitter/index.html"
+  }
+];
